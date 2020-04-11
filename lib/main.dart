fb4fb4a1b30b85bdbf6f920a8cfb96e3465e9e2e
Yul Jae Lee <yuljlee9@gmail.com>
2020-04-10 22:25:00 -0700

    feat: Add icon button

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,56 @@ class MyApp extends StatelessWidget {
         ),
         body: Container(
           margin: EdgeInsets.all(50.0),
+          //alignment: Alignment.center,
           //color: Colors.green,
           decoration: BoxDecoration(
-            color: Colors.green,
-            border: Border.all(),
-            //shape: BoxShape.circle,
+            //color: Color(0xFF006666),
+            //color: Colors.pink,
+            border: Border.all(2),
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
           ),
-          child: Text('Hello', style: Theme.of(context).textTheme.headline),
-          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //Text('Back', style: Theme.of(context).textTheme.headline),
+              //Text('Click here to flip front',
+              //    style: Theme.of(context).textTheme.body1),
+              Spacer(),
+              Container(
+                color: Colors.white,
+                child: Text('Back', style: Theme.of(context).textTheme.headline),
+              ),
+              Container(
+                color: Colors.orange,
+                child: Text('Click here to flip front', style: Theme.of(context).textTheme.body1),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Ink(
+                          decoration: const ShapeDecoration(
+                            color: Colors.lightBlue,
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                                          icon: Icon(Icons.volume_up),
+                                          iconSize: 40,
+                                          tooltip: 'Listen!',
+                                          splashColor: Colors.purple,
+                                          onPressed: () {
+                                            print('IconButton is pressed');
+                                          }
+                                        ),
+                        )
+                                 
+                               
+                ),
+              ),
+            ],
+          ),
+          
         ),
       ),
     );
